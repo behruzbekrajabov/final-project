68a863a2a45689d1ff5fabd6aec06c4578585468
Behruzbek Rajabov <behrouz.rajabov911574210@gmail.com>
2025-12-03 17:10:48 +0500

    second commit

--- app.js
+++ app.js
@@ -1,10 +1,20 @@
 import TelegramBot from "node-telegram-bot-api";
 import "./src/bot/bot.js"
 import dotenv from "dotenv";
-
+import mongoose from "mongoose";
 
 dotenv.config();
-
+async function connectDB() {
+    mongoose.connect(process.env.MONGOOSE_URI)
+    .then(() =>{
+        console.log("🎆DB connected🎆");
+        
+    })
+    .catch(() =>{
+        console.log("Error: DB didn`t connect!!!");
+        
+    })
+}
 
 
 
--- src/bot/bot.js
+++ src/bot/bot.js
@@ -7,6 +7,7 @@ export const bot = new TelegramBot(process.env.BOT_TOKEN, { polling:true })
 bot.on("message", function(msg){
     const chatID = msg.chat.id;
     const firstname = msg.chat.first_name;
+    // const lastName = msg.chat.last_name;
     bot.sendMessage(chatID, `Hello, ${firstname}`)
 })
 
